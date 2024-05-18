04fe626a8bd425002dba9539cfb6b99ed28301ca
Thea <uythea487102@gmail.com>
2024-05-18 18:03:08 +0800

    Created a class named TV

--- television_settings.py
+++ television_settings.py
@@ -4,6 +4,20 @@
 
 
 #create a class named TV
+class TV():
+
+    #parameter constructor where in:
+    #status defines the current state of the television (on/off)
+    #volume refers to the volume of the television
+    #channel_list has a channel from 1 to 120
+    #current_channel has no current channel
+
+    def __init__(self, status = "OFF", volume = 0, channel_list = list(range(1,121)), current_channel = None):
+
+        self.status = status
+        self.volume = volume
+        self.channel_list = channel_list
+        self.current_channel = current_channel
 
 #define the parameters of the constructor
 
