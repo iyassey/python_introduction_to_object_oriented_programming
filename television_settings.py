59e6403086b70e5c6578123f4e348006f6c5188f
Thea <uythea487102@gmail.com>
2024-05-18 17:45:07 +0800

    Pseudocode

---
+++ television_settings.py
@@ -0,0 +1,17 @@
+#author__thea_uy
+#date_may_05_2024
+#Create a Python Code for creating the Class named TV and a Test Driver program named TestTV that will create two objects from Class TV 
+
+
+#create a class named TV
+
+#define the parameters of the constructor
+
+
+#create a method that turns on the television
+
+#create a method that turns off the television
+
+#create a method that allows that user to change the volume settings
+
+#create a method that alllows that user to change the channel settings
